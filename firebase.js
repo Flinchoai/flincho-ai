cd10c1744199224a00919156fa681ac22bda95fe
Flinchoai <shakesscarlock@gmail.com>
2026-07-10 12:41:40 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,15 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+<input type="email" id="email" placeholder="Email">
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDl9fQ0cQgP-k7MM9GZHQtXC0SLwAgzjso",
-  authDomain: "flincho-ai.firebaseapp.com",
-  projectId: "flincho-ai",
-  storageBucket: "flincho-ai.firebasestorage.app",
-  messagingSenderId: "329462520131",
-  appId: "1:329462520131:web:b5c38fa19a8db98499b7f6"
-};
+<br><br>
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+<input type="password" id="password" placeholder="Password">
 
-window.auth = auth;
-window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
-window.signInWithEmailAndPassword = signInWithEmailAndPassword;
-window.signOut = signOut;
+<br><br>
+
+<button id="loginBtn">Login</button>
+
+<button id="registerBtn">Create Account</button>
+
+<p id="message"></p>
+
+<script type="module" src="firebase.js"></script>
